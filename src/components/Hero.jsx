770f45ad0b36002ac8d8ac99667e10cf0f3d0930
NakardaRichards <nakardarichards3@gmail.com>
2023-05-09 01:29:21 -0500

    uno

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src="src\assets\SummAIze.png" alt='logo' className='w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/NakardaRichards/summaize", "_blank")
+          }
+          className='git_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+      Effortlessly Summarize Lengthy Articles with <b style={{color:"#84D0FF"}}>SummAIze</b>
+      </h1>
+  
+    </header>
+  );
+};
+
+export default Hero;
